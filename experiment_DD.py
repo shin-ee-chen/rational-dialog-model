062582236c76b3592c17d982ae7aaa716d5797ad
FrankVerhoef <fsa.verhoef@chello.nl>
2021-05-13 15:44:50 +0200

    Added perturbation function to UtterancesDataset class

--- experiment_DD.py
+++ experiment_DD.py
@@ -7,26 +7,12 @@ from daily_dialog.DialogTokenizer import get_daily_dialog_tokenizer
 import datasets
 
 
-original_dataset = datasets.load_dataset("daily_dialog", split="train", )
-example = original_dataset[0]["dialog"]
-print("----------\nExample in original dataset\nLength = ", len(example), "\n", example)
-
-def subs(utterances):
-    num = len(utterances)
-    if num < 2:
-        return None
-    subsets = [
-        ('[SEP]'.join(utterances[start:end]), utterances[end])
-        for start in range(0, num-1)
-        for end in range(start+1, num)
-    ]
-    return subsets
-
-# for i, u in enumerate(subs(example)):
-#     print(i, u)
+# original_dataset = datasets.load_dataset("daily_dialog", split="train", )
+# example = original_dataset[0]["dialog"]
+# print("----------\nExample in original dataset\nLength = ", len(example), "\n", example)
 
 
-batch_size = 256
+batch_size = 8
 my_tokenizer = get_daily_dialog_tokenizer(tokenizer_location='./daily_dialog/tokenizer.json', )
 
 # tokenized_example = my_tokenizer.encode('[SEP]'.join(example)).ids
@@ -34,18 +20,19 @@ my_tokenizer = get_daily_dialog_tokenizer(tokenizer_location='./daily_dialog/tok
 # decoded_example = my_tokenizer.decode(tokenized_example)
 # print("decoded:   ", decoded_example)
 
-dataset_train = UtterancesDataset(my_tokenizer, split="train", batch_size=batch_size, subsets="full")
-dataloader_train = DataLoader(dataset_train, batch_size=1, )
+dataset_train = UtterancesDataset(my_tokenizer, split="train", subsets="start", perturbation="words_dialogue", size=1)
+dataloader_train = DataLoader(dataset_train, batch_size=batch_size, collate_fn=UtterancesDataset.collate_fn)
 
 def verify_batches(dl):
     bnum = 0
     for batch in dl:
-        print("Batch #",bnum, "len: ", len(batch), "min: ", len(batch[0][0]), "max: ", len(batch[-1][0]))
-        (context, response) = batch[0]
+        print("Batch #",bnum, "len: ", len(batch[0]))
+        (contexts, responses) = batch
         print("---------- ",bnum)
-        print("\n", context, '\n', response)
-        print("Context: ", my_tokenizer.decode(context))
-        print("Response:", my_tokenizer.decode(response))
+        for i in range(len(contexts)):
+            print(i, contexts[i], responses[i])
+            print("Context: ", my_tokenizer.decode(contexts[i].tolist()))
+            print("Response:", my_tokenizer.decode(responses[i].tolist()))
         bnum += 1
 
-#verify_batches(dataloader_train)
+verify_batches(dataloader_train)
